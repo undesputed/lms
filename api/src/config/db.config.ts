25c5e0b7a0395e1c57276975cb7ecd1f70ec0d6a
carrie <carrie@bpoc.co.jp>
2023-11-29 16:49:15 +0800

    updates7

--- api/src/config/db.config.ts
+++ api/src/config/db.config.ts
@@ -7,9 +7,9 @@ export interface DatabaseConfig {
 }
 
 export const databaseConfig: DatabaseConfig = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USERNAME,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
+  HOST: process.env.DB_HOST ? process.env.DB_HOST : 'ec2-13-229-122-211.ap-southeast-1.compute.amazonaws.com',
+  USER: process.env.DB_USERNAME ? process.env.DB_USERNAME : 'root',
+  PASSWORD: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : '',
+  DB: process.env.DB_NAME ? process.env.DB_NAME : 'ec_care',
   PORT: process.env.DB_PORT ? process.env.DB_PORT : 3306,
 };
